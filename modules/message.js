de87297d100d46d609021f83fdc2367416a583d1
bmson7112 <sonm6976@gmail.com>
2023-12-28 00:32:16 +0700

    add const

--- modules/message.js
+++ modules/message.js
@@ -1,5 +1,24 @@
 // general
-export const alarm = {
+export const ALARM = {
     time: "7:00:00 AM",
     message: "𝗟𝗢𝗔𝗟𝗢𝗔𝗟𝗢𝗔 📢📢📢\nDậy đi làm đi các con vợ ☀️📣 mất hơn 6 lít bây giờ :)\n@hoangndst @amunn35 @tuda_2 @sonbm1 @crvt4722 @duongtm3 @ndvinhcn",
+}
+export const LUNCH = {
+    time: "8:00:00 AM",
+    message: "Đặt cơm đi các con vợ, quên là ra ngoài ăn cơm tấm nhé! 🙂🍚🍌"
+}
+
+export const LEAVE = {
+    time: "5:30:00 AM",
+    message: "Đến giờ về rồi, về thôi các người anh em 😏, không về thì xuống A1Y cơ sở tầng 12 để high nào 🙂"
+}
+
+export const HIGH_1 = {
+    time: "9:30:00 AM",
+    message: "Đến giờ đi ngắm trời ngắm mây rồi các người anh em 😶‍🌫️. Tầng 12 nhé!"
+}
+
+export const HIGH_2 = {
+    time: "3:30:00 PM",
+    message: "Đến giờ đi ngắm trời ngắm mây rồi các người anh em 😶‍🌫️. Tầng 12 nhé!"
 }
