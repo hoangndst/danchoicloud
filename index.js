36658b4358cdfc007b8c063b3a002db92d4a229a
Nguyễn Đình Hoàng <hoangnd40@viettel.com.vn>
2023-12-28 21:08:10 +0700

    release/v0.1.0

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-import { a1y } from './modules/channel.js';
-import { sendMessage } from './modules/bot.js';
+import { scheduleWeekday } from "./schedule/daily.js";
 
-const options = {
-  chat_id: a1y.chat_id,
-  text: 'Dậy học tư tưởng đi @tuda_2',
-  message_thread_id: a1y.topics[0].message_thread_id,
-};
-
-// catch promise
-sendMessage(options, true).catch((error) => {
-  console.log(error);
-} );
+scheduleWeekday().then(() => {
+  console.log("Schedule weekday successfully");
+}).catch((error) => {
+  console.log("Error scheduling weekday: ", error);
+})
