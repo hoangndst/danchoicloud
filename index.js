ff4549d3b50e99e5a3ad4e9b0a57f3a0f815ff27
hoangndst <hoangndst@gmail.com>
2024-04-01 22:45:23 +0700

    disable a1y alert

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 import { scheduleWeekday, scheduleDay, scheduleRoutine } from "./schedule/daily.js";
 import { features } from "./features/message.js";
 
-scheduleDay().then(() => {
-  console.log("Schedule day successfully");
-}).catch((error) => {
-  console.log("Error scheduling day: ", error);
-})
+// scheduleDay().then(() => {
+//   console.log("Schedule day successfully");
+// }).catch((error) => {
+//   console.log("Error scheduling day: ", error);
+// })
 
-scheduleWeekday().then(() => {
-  console.log("Schedule weekday successfully");
-}).catch((error) => {
-  console.log("Error scheduling weekday: ", error);
-})
+// scheduleWeekday().then(() => {
+//   console.log("Schedule weekday successfully");
+// }).catch((error) => {
+//   console.log("Error scheduling weekday: ", error);
+// })
 
 scheduleRoutine().then(() => {
   console.log("Schedule routine successfully");
